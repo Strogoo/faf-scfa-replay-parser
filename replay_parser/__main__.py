b6f01195d100633c0aa90ac8c63dd5a29624c474
Strogoo <Pilot9.65@gmail.com>
2020-02-18 23:58:15 +0300

    1

--- replay_parser/__main__.py
+++ replay_parser/__main__.py
@@ -9,6 +9,9 @@ from body import ReplayBody
 from reader import ReplayReader
 from replay import continuous_parse, parse
 
+REPLAYS_DIR = "D:/replays"
+replayIDs = {}
+
 def get_scfa_replay_by_data(data: bytes):
     """
     FAF and Supreme Commander has 2 kind of formats:
@@ -27,12 +30,26 @@ def get_scfa_replay_by_data(data: bytes):
 
     return data
 
-file = open("D:/11052825.fafreplay", "rb").read()
+def get_replays_in_dir():
+    replays = []
+    n = 1
+    for file_name in os.listdir(REPLAYS_DIR):
+        if file_name.endswith(".fafreplay"):
+            replayIDs[n] = file_name
+            n = n + 1
+            file = open(os.path.join(REPLAYS_DIR, file_name), "rb").read()
+            scfa = get_scfa_replay_by_data(file)
+            replays.append(scfa)
+    return replays
 
-scfa = get_scfa_replay_by_data(file)
+#file = open("D:/replays/test.fafreplay", "rb").read()
 
-data = parse(
-        scfa,
+#scfa = get_scfa_replay_by_data(file)
+
+replayNum = 1
+for replay in get_replays_in_dir():
+    data = parse(
+        replay,
         True,
         parse_commands={
             CommandStates.Advance,
@@ -41,9 +58,38 @@ data = parse(
             CommandStates.VerifyChecksum,
             CommandStates.LuaSimCallback,
             CommandStates.IssueCommand,
+            CommandStates.ProcessInfoPair,
         }
     )
+    players = {}
+    for k in data['header']['armies'].keys():
+        players[k] = data['header']['armies'][k]['PlayerName']
+    print(players)
+    #break
+    lookForAbuse = False
+    n = 1
+    for bd in data['body']:
+        if n < 100000:
+            n = n + 1
 
-for bd in data['body']:
-    print(bd)
-    print("------------------------------------------------------------------------------------")
+            for key in bd.keys():
+                playerCommands = bd[key]
+                for com in playerCommands.keys():
+                    if lookForAbuse and com == "ProcessInfoPair":
+                        if playerCommands[com]["arg1"] == "ToggleScriptBit":
+                            print(str(n) + " " + players[key] + " " + playerCommands[com]["arg1"] + " " + playerCommands[com]['arg2'])
+
+                    if com == "IssueCommand":
+                        # print(playerCommands[com].keys())
+
+                        if playerCommands[com]['cmd_data']['blueprint_id'] == "xrb3301":
+                            print(replayNum)
+                            print("TICK:" + str(n))
+                            print("PLAYER:" + players[key])
+                            print(playerCommands[com]['cmd_data'])
+                            lookForAbuse = True
+                        # print("-------")
+    replayNum = replayNum + 1
+
+print(replayIDs)
+    #print("------------------------------------------------------")
